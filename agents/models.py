02951f147e611d65b60518c98bbb0b96ddbd1b54
simplykevine <kevineumutonibugingo@gmail.com>
2025-09-08 11:45:11 +0300

    created an agent model

--- agents/models.py
+++ agents/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Agent(models.Model):
     agent_id = models.AutoField(primary_key=True)
-    agent_name = models.CharField(max_length=255)
+    agent_name = models.CharField(max_length=255, unique=True)
     description = models.TextField()
 
     def __str__(self):
